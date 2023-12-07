0a26e414eb0211ad52689aafdd2de0e702f4e663
chantelle mckenzie <mrspasceri@hotmail.com>
2023-12-06 20:05:48 -0800

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,48 @@
-import { useState } from 'react'
-import './App.css'
-import foods from './data/foods'
-import FoodList from './components/FoodList'
+import { useState } from "react";
+import "./App.css";
+import foods from "./data/foods";
+import FoodList from "./components/FoodList";
 
 function App() {
-  const [foodItems, setFoodItems] = useState(foods[0])
-  const [search, setSearch] = useState('');
+  const [foodItems, setFoodItems] = useState(foods[0]);
+  const [search, setSearch] = useState("");
 
   const foodSelected = (food) => {
-    setFoodItems(food)
-  }
+    setFoodItems(food);
+  };
 
   const handleSearch = () => {
     return foods.filter((food) =>
-    food.name.toLowerCase().includes(search.toLowerCase())
+      food.name.toLowerCase().includes(search.toLowerCase())
     );
   };
 
   return (
-  
-      <div className="container">
-        <div className="row">
+    <div className="container">
+      <div className="row">
         <div className="col-md-4 col-lg-3">
-            <h1 className="display-4">Food Items</h1>
-          </div>
-    <div className="col-md-8 col-lg-9">
-            <input 
-            type='text'
+          <h1 className="display-4">Food Items</h1>
+        </div>
+        <div className="col-md-8 col-lg-9">
+          <input
+            type="text"
             className="form-control"
-            placeholder='Search'
+            placeholder="Search"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
+          />
+
+          <div className="row">
+            <FoodList
+              foods={handleSearch()}
+              foodSelected={foodSelected}
+              search={search}
             />
-      
-           <div className='row'>
-            <FoodList foods={handleSearch()} foodSelected={foodSelected} search={search}/>
-          </div>
           </div>
         </div>
       </div>
-
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
 
-function FoodList({ foods, foodSelected }) {
+function FoodList({ foods, foodSelected, search }) {
+  //function to highlight the search text the user imputs
+  //stackoverflow for help on this one!
+  const highlightSearch = (text) => {
+    if (search) {
+      const regex = new RegExp(`(${search})`, "gi");
+      return text
+        .split(regex)
+        .map((word, i) =>
+          regex.test(word.toLowerCase()) ? <mark key={i}>{word}</mark> : word
+        );
+    } else {
+      return text;
+    }
+  };
+
   return (
     <ul className="list-group">
       {foods.map((food) => (
         <li key={food.id} onClick={() => foodSelected(food)}>
-     
-  
-       {food.name} {food.description && `- ${food.description}`}
+          {highlightSearch(food.name)}
+          <span className="mx-1"> - {highlightSearch(food.description)}</span>
         </li>
       ))}
     </ul>
