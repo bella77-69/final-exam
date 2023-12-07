7a3a456c3dd706c04a4d6de054495ba4ff997ee5
chantelle mckenzie <mrspasceri@hotmail.com>
2023-12-06 20:31:19 -0800

    added some comments around the functions

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -4,14 +4,26 @@ function FoodList({ foods, foodSelected, search }) {
   //function to highlight the search text the user imputs
   //stackoverflow for help on this one!
   const highlightSearch = (text) => {
+    // Check for seach 
     if (search) {
+      // Create a regular expression 
       const regex = new RegExp(`(${search})`, "gi");
+      // Split the text into an array of words
       return text
         .split(regex)
+        // Map over each word in the array
         .map((word, i) =>
-          regex.test(word.toLowerCase()) ? <mark key={i}>{word}</mark> : word
+          // Check if the word matches the search 
+          regex.test(word.toLowerCase()) ? (
+            // If it does match, put the word in a <mark> for highlighting
+            <mark key={i}>{word}</mark>
+          ) : (
+            // If not matched, return the original word
+            word
+          )
         );
     } else {
+      // If no search term is provided, return the original text without highlighting
       return text;
     }
   };
