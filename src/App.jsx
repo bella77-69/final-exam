3e6fad791244887b1ece056a76d82c0bb9cbc0c8
chantelle mckenzie <mrspasceri@hotmail.com>
2023-12-06 18:18:41 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import FoodItem from './components/FoodItem'
+
+function App() {
+
+  return (
+    <>
+<FoodItem />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/FoodItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FoodItem(props) {
+    return (
+        <div>
+            Single Food Item
+        </div>
+    );
+}
+
+export default FoodItem;
